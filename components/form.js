13acf25bccaddfc3a7bf82750e2f18e455e461c3
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-07 20:36:03 +0100

    twick form

--- components/form.js
+++ components/form.js
@@ -4,21 +4,21 @@ import { placeholder } from "./placeholder";
 
 const InitialForm = () => {
     const formik = useFormik({
-        initialValues: { Text: "" },
+        initialValues: { question: "" },
         onSubmit: values => {
           alert(JSON.stringify(values, null, 2));
         }
       });
       return (
        <form onSubmit={formik.handleSubmit} >
-          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">Email Address</label>
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="question">Ask Whatever</label>
           <input
-            id="email"
-            name="email"
-            type="email"
+            id="question"
+            name="question"
+            type="text"
             placeholder={placeholder()}
             onChange={formik.handleChange}
-            value={formik.values.email}
+            value={formik.values.question}
             className=" block w-full bg-transparent text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
           <button type="submit">Submit</button>
