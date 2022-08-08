07b65047e7a123ee340fce8f4271e3ab80ce568b
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-08 15:01:53 +0100

    fix env

--- components/worker.js
+++ components/worker.js
@@ -1,14 +1,13 @@
 const { Configuration, OpenAIApi } = require("openai");
 import Loader from "./spinner"
 
-
-
 export default async function worker(question){
   let result = "";
   let isMounted = false;
+  const token = process.env.OPENAI
 const configuration = new Configuration({
 
-  apiKey: process.env.OPENAI,
+  apiKey: token,
 
 });
 const openai = new OpenAIApi(configuration);
@@ -22,9 +21,6 @@ const completion = await openai.createCompletion({
   frequency_penalty: 0.2,
   presence_penalty: 0,
 });
-// let result = "";
-//     completion.data.choices.forEach(elements => result+=elements.text);
-//     console.log(result);
 if (completion.data.choices.length !== 0) {
     completion.data.choices.forEach(elements => result+=elements.text);
     console.log(result);
